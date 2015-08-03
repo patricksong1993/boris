543f62fb632733adee702ef61ef468f7b8a52023
David Harver Pollak <bebus77@gmail.com>
2015-08-03 20:37:25 +0100

    backend api initial commit, spring mvc, hibernate and mysql remote connection

---
+++ api/src/main/java/com/blackrock/boris/dto/Article.java
@@ -0,0 +1,49 @@
+package com.blackrock.boris.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "articles")
+public class Article {
+
+	@Id
+	@Column(name="id")
+	private long id;
+	@Column(name="link")
+	private String link;
+	@Column(name="source")
+	private String source;
+	@Column(name="title")
+	private String title;
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+
+}
