9dfd321db3de04b2aa534a8de158a02c278db2bb
David Harver Pollak <bebus77@gmail.com>
2015-08-04 15:12:37 +0100

    baisheng apple watch

--- api/src/main/java/com/blackrock/boris/utilities/technologies/TechnologyImporter.java
+++
@@ -1,9 +0,0 @@
-package com.blackrock.boris.utilities.technologies;
-
-import com.blackrock.boris.dto.Technology;
-
-import java.util.Collection;
-
-public interface TechnologyImporter {
-    public void importTechnologies();
-}
