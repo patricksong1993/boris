1ad79d9e23b8a75a0d6edc835300bff90de34c46
David Harver Pollak <bebus77@gmail.com>
2015-08-04 15:29:12 +0100

    boris files

---
+++ src/main/java/com/blackrock/boris/controllers/TechnologyController.java
@@ -0,0 +1,37 @@
+package com.blackrock.boris.controllers;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.blackrock.boris.dto.Technology;
+import com.blackrock.boris.exceptions.BorisInternalException;
+import com.blackrock.boris.services.TechnologyService;
+
+@Controller
+public class TechnologyController {
+
+    private TechnologyService technologyService;
+
+    @RequestMapping(value = "toptechnologies", method = RequestMethod.GET)
+    public List<Technology> getTopTechnologies() throws BorisInternalException {
+        List<Technology> trendingTechnologies = technologyService.getTrendingTechnologies();
+
+        return trendingTechnologies;
+    }
+
+    @RequestMapping(value = "tech/forName", method = RequestMethod.GET)
+    public Technology getTechnologyForName(@PathVariable("technologyName") String technologyName) throws BorisInternalException {
+        Technology technology = technologyService.getTechnologyForName(technologyName);
+
+        return technology;
+    }
+
+    public void setTechnologyService(TechnologyService technologyService) {
+        this.technologyService = technologyService;
+    }
+}
+
--- src/main/java/com/blackrock/boris/services/TechnologyService.java
+++ src/main/java/com/blackrock/boris/services/TechnologyService.java
@@ -7,15 +7,22 @@ import com.blackrock.boris.dto.Technology;
 import com.blackrock.boris.exceptions.BorisInternalException;
 
 public class TechnologyService {
-	
-	TechnologyDao technologyDao;
-	
-	public List<Technology> getTrendingTechnologies() throws BorisInternalException {
-		List<Technology> trendingTech = technologyDao.getTrendingTechnologies();
-		return trendingTech;
-	}
-	
-	public void setTechnologyDao(TechnologyDao technologyDao) {
-		this.technologyDao = technologyDao;
-	}
+
+    TechnologyDao technologyDao;
+
+    public List<Technology> getTrendingTechnologies() throws BorisInternalException {
+        List<Technology> trendingTech = technologyDao.getTrendingTechnologies();
+        return trendingTech;
+    }
+
+    public Technology getTechnologyForName(String technologyName) throws BorisInternalException {
+        Technology technology = technologyDao.getTechnologyForName(technologyName);
+
+        return technology;
+    }
+
+    public void setTechnologyDao(TechnologyDao technologyDao) {
+        this.technologyDao = technologyDao;
+    }
+
 }
