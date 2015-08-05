a11e5488b95e02e8f3a10c2a1cfec46c9b26b8f9
Baisheng Song <Baisheng@Baishengs-MacBook-Pro.local>
2015-08-05 12:41:40 +0100

    ajax add article

--- src/main/webapp/resources/core/js/dialog.js
+++ src/main/webapp/resources/core/js/dialog.js
@@ -72,39 +72,6 @@ angular.module('MyApp')
     .controller('addArticleControl', function($scope, $mdDialog) {
         $scope.alert = '';
 
-        //$scope.showAlert = function(ev) {
-        //    // Appending dialog to document.body to cover sidenav in docs app
-        //    // Modal dialogs should fully cover application
-        //    // to prevent interaction outside of dialog
-        //    $mdDialog.show(
-        //        $mdDialog.alert()
-        //            .parent(angular.element(document.body))
-        //            .title('This is an alert title')
-        //            .content('You can specify some description text in here.')
-        //            .ariaLabel('Alert Dialog Demo')
-        //            .ok('Got it!')
-        //            .targetEvent(ev)
-        //    );
-        //};
-        //
-        //$scope.showConfirm = function(ev) {
-        //    // Appending dialog to document.body to cover sidenav in docs app
-        //    var confirm = $mdDialog.confirm()
-        //        .parent(angular.element(document.body))
-        //        .title('Would you like to delete your debt?')
-        //        .content('All of the banks have agreed to forgive you your debts.')
-        //        .ariaLabel('Lucky day')
-        //        .ok('Please do it!')
-        //        .cancel('Sounds like a scam')
-        //        .targetEvent(ev);
-        //
-        //    $mdDialog.show(confirm).then(function() {
-        //        $scope.alert = 'You decided to get rid of your debt.';
-        //    }, function() {
-        //        $scope.alert = 'You decided to keep your debt.';
-        //    });
-        //};
-
         $scope.showAdvanced = function(ev) {
             $mdDialog.show({
                 controller: DialogController,
@@ -138,38 +105,6 @@ angular.module('MyApp')
 
     .controller('addCalendarControl', function($scope, $mdDialog) {
         $scope.alert = '';
-        //$scope.showAlert = function(ev) {
-        //    // Appending dialog to document.body to cover sidenav in docs app
-        //    // Modal dialogs should fully cover application
-        //    // to prevent interaction outside of dialog
-        //    $mdDialog.show(
-        //        $mdDialog.alert()
-        //            .parent(angular.element(document.body))
-        //            .title('This is an alert title')
-        //            .content('You can specify some description text in here.')
-        //            .ariaLabel('Alert Dialog Demo')
-        //            .ok('Got it!')
-        //            .targetEvent(ev)
-        //    );
-        //};
-        //
-        //$scope.showConfirm = function(ev) {
-        //    // Appending dialog to document.body to cover sidenav in docs app
-        //    var confirm = $mdDialog.confirm()
-        //        .parent(angular.element(document.body))
-        //        .title('Would you like to delete your debt?')
-        //        .content('All of the banks have agreed to forgive you your debts.')
-        //        .ariaLabel('Lucky day')
-        //        .ok('Please do it!')
-        //        .cancel('Sounds like a scam')
-        //        .targetEvent(ev);
-        //
-        //    $mdDialog.show(confirm).then(function() {
-        //        $scope.alert = 'You decided to get rid of your debt.';
-        //    }, function() {
-        //        $scope.alert = 'You decided to keep your debt.';
-        //    });
-        //};
 
         $scope.showAdvanced = function(ev) {
             $mdDialog.show({
@@ -180,6 +115,20 @@ angular.module('MyApp')
             })
                 .then(function(answer) {
                     $scope.alert = 'You said the information was "' + answer + '".';
+
+                    var title = (".add-tech-title")[0].value;
+                    var desc = $(".add-tech-desc")[0].value;
+
+                    var obj = $.parseJSON('{ "title" : '+title+', "desc" : '+desc+' }');
+                    $.ajax({
+                        type: "POST",
+                        url: "boris/articles",
+                        data: obj,
+                        success: success,
+
+                    }).done(function(){
+                        alert("done");
+                    })
                 }, function() {
                     $scope.alert = 'You cancelled the dialog.';
                 });
