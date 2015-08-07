49ba59b5f48d7652298ad2b10ae21e05e140201f
alaromeny <sd246@hw.ac.uk>
2015-08-07 19:33:06 +0100

    Fixed the favorites and the favorites filter

--- src/main/webapp/resources/core/js/test.js
+++ src/main/webapp/resources/core/js/test.js
@@ -3,39 +3,47 @@
             $scope.myData = {};
 
 
-            $scope.subFilterBool = false;
-            $scope.subFilterString = '';
-
-
-            $scope.subFilter = function(){
-                if($scope.subFilterBool == false){
-                    $scope.subFilterString = '| filter:{subscribed:false}';
-                    $scope.subFilterBool = !$scope.subFilterBool;
-                }
-                else{
-                    $scope.subFilterString = '';
-                    $scope.subFilterBool = !$scope.subFilterBool;
-                }
-
-
-
-            }    
+            $scope.subFilterTracker = false;
+            $scope.subFilter = '';  
 
             $scope.sub = function(j){
 
-                $scope.myData[j].subscribed = !$scope.myData[j].subscribed;
+                if($scope.myData[j].subscribed == 'false'){
+
+                    $scope.myData[j].subscribed = 'true';
+                    console.log($scope.myData[j].title +" "+ $scope.myData[j].subscribed);
+                    console.log('Filter: '+$scope.subFilter);
+                }
+                else{
+                    $scope.myData[j].subscribed = 'false';
+                    console.log($scope.myData[j].title +" "+ $scope.myData[j].subscribed);
+
+                }
+            
+            }
+
+            $scope.filterTrending = function(){
+
+                if($scope.subFilterTracker == false){
+                    $scope.subFilter = 'true';
+                    $scope.subFilterTracker = true;
+                    console.log('Debug1: subFilter: '+$scope.subFilter+"Tracker: "+ $scope.subFilterTracker);
+
+                }
+                else if ($scope.subFilterTracker == true){
+                    $scope.subFilter = '';
+                    $scope.subFilterTracker = false;
+                    console.log('Debug2: subFilter: '+$scope.subFilter+"Tracker: "+ $scope.subFilterTracker);
+                }
 
             }
 
+
             $http.get("technologies/trending").success(function(data, status, headers, config) {
                     $scope.myData = data;
-                    $scope.myData[0].subscribed = true;
-                    for (var j = 1; j < $scope.myData.length; j++){
-                      $scope.myData[j].subscribed = !$scope.myData[j-1].subscribed;
-
+                    for (var j = 0; j < $scope.myData.length; j++){
+                      $scope.myData[j].subscribed = 'false';
                       console.log($scope.myData[j].title);
-                      
-                      console.log($scope.myData[j].subscribed);
                     }
                     
                 }).error(function(data, status, headers, config) {
